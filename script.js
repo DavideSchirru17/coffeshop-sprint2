fffa90c8c8013cb6e205cf1b2139d9bb5e5b0002
cantinaski <125932510+cantinaski@users.noreply.github.com>
2023-11-17 18:51:47 +0000

    modifying position absolute to position relative

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const cards = document.querySelectorAll('.cardContainer__card');
 
 cards.forEach((card) => {
     card.addEventListener('click', function() {
-            card.style.boxShadow =  '0px 7px 50px 35px red';
+            card.style.boxShadow =  '0px 7px 50px 35px #03e9f4';
    
     setTimeout(function(){
         card.style.boxShadow = '0px 5px 9px 1px rgba(0, 0, 0, 0.25)';
